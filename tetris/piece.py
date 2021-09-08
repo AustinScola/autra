65cacd99373620a7d4a63faa0a46667d3ff80301
Austin Scola <austinscola@gmail.com>
2021-09-08 18:38:23 -0400

    Fix piece rotations type annotation

--- tetris/piece.py
+++ tetris/piece.py
@@ -7,7 +7,7 @@ from tetris.rotation import Rotation
 
 class Piece:  # pylint: disable=too-few-public-methods
     """A tetromino piece."""
-    rotations = Dict[Orientation, Rotation]
+    rotations: Dict[Orientation, Rotation]
 
     def __init__(self, orientation: Orientation) -> None:
         self.orientation = orientation
