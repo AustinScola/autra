4b268f15234d75869859af9f10876b9ef188fbb8
Austin Scola <austinscola@gmail.com>
2021-09-09 15:41:44 -0400

    fixup filled playfield

--- test_data/playfields.py
+++ test_data/playfields.py
@@ -1,4 +1,4 @@
 """Some playfields used in tests."""
 from tetris.state import COLUMNS, ROWS, Playfield
 
-FilledPlayfield = Playfield(tuple((False, ) * COLUMNS for _ in range(ROWS)))
+FilledPlayfield = Playfield(tuple((True, ) * COLUMNS for _ in range(ROWS)))
